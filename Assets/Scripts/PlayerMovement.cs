cbc173f3276acbb3ed36df9217d3609e6ee40413
døffer <christoffer.b.thomsen@gmail.com>
2024-12-07 00:11:07 +0100

    Movement Scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [Header("Player Movement")]
     public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+
+    //Implemented to check if the player is grounded, needed to implement drag.
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
 
     public Transform orient;
 
@@ -19,12 +37,86 @@ public class NewBehaviourScript : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        readyToJump = true;
     }
+
+    //Using FixedUpdate instead, as putting this into Update accelerates the player far more, bordering on uncontrollable.
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void Update()
+      {
+        //Checking if the player is grounded, if so, drag is applied to the player.
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        LimitSpeed();
+
+        //If the player is grounded, drag is applied to the player.
+        if (grounded)
+            rb.drag = groundDrag;
+    
+        else
+            rb.drag = 0;
+
+      }
 
     private void MyInput()
     {
        horiInput = Input.GetAxisRaw("Horizontal");
        vertInput = Input.GetAxisRaw("Vertical");
+
+       if(Input.GetKey(jumpKey) && readyToJump && grounded)
+       {
+            Jump();
+            readyToJump = false;
+            
+            //Used for continuous jumping, as the player can jump again after the cooldown.
+            Invoke(nameof(ResetJump), jumpCooldown);
+       }
+    }
+    private void LimitSpeed()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed * 1.25f)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed * 1.25f;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+    
+    private void MovePlayer()
+    {
+        moveDirection = orient.forward * vertInput + orient.right * horiInput;
+
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            // Limit the speed to 1.5 times the max speed when in the air
+            if (rb.velocity.magnitude < moveSpeed * 1.5f)
+            {
+                rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier * 10f, ForceMode.Force);
+            }
+        }
+    }
+
+    
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 
 }
